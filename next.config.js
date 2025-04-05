b2399a20ec181d33279e7639831b625a4026030b
misae717 <116470844+misae717@users.noreply.github.com>
2025-04-05 12:07:47 +0300

    Initial commit of Game Jam Portal

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/GameJamShowcase' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/GameJamShowcase/' : '',
   trailingSlash: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig; 
